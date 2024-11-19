c4a484eda790bfbe400803190d18e82e61df3abe
jh080724 <tsj010813@gmail.com>
2024-11-19 11:20:35 +0900

    웹훅 테스트 푸쉬를 위해 코드수정2.

--- app/src/main/java/com/study/controller/HomeController.java
+++ app/src/main/java/com/study/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello Jenkins! 우헤헤헤~~";
+        return "Hello Jenkins! 우헤헤헤~~ 또 실패했네~ 푸하하하하";
     }
 }
